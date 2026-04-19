8b4f578d324e5b535cf0551fd62c42fe0d1361d2
ricajus <ricartoonstudio@gmail.com>
2026-04-19 20:43:06 -0300

    feat: add repositories

---
+++ src/main/java/com/ricajust/escola/repositories/MatriculaRepository.java
@@ -0,0 +1,9 @@
+package com.ricajust.escola.repositories;
+
+import com.ricajust.escola.entities.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatriculaRepository extends JpaRepository<Matricula, Integer> {
+}
---
+++ src/main/java/com/ricajust/escola/repositories/TurmaRepository.java
@@ -0,0 +1,9 @@
+package com.ricajust.escola.repositories;
+
+import com.ricajust.escola.entities.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Integer> {
+}
